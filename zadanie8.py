6fc06f597184d614eac7d71d7ffdb84ea577779b
mylosz93 <75523962+mylosz93@users.noreply.github.com>
2020-12-05 14:33:04 +0100

    zlycommit

--- zadanie8.py
+++
@@ -1,8 +0,0 @@
-lista_pilkarzy = ['Putnocky','Pawelec','Celeban','Stiglec','Puerto','Sobota']
-print(len(lista_pilkarzy))
-print(lista_pilkarzy[-1])
-
-for i in lista_pilkarzy:
-    print(i)
-for nr in range(len(lista_pilkarzy)):
-    print("nr: " + str(nr) + " : " + lista_pilkarzy[nr])
